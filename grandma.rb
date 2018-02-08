c5ecebbae3e4cc001e2d147d5eeb7a350997df8c
Tatyana Scotce <tscotce@gmail.com>
2018-02-08 00:15:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
