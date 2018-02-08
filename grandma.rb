c8b16d55fc147db097aef109883d767926b410bd
Tatyana Scotce <tscotce@gmail.com>
2018-02-08 00:29:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else puts "HUH?! SPEAK UP, SONNY!"
+  else return "HUH?! SPEAK UP, SONNY!"
   end
 end
